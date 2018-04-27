53f836af89691b83caf0b9f669d9bd7d0a413d0b
ElRaptorus <christian.werner@5minds.de>
2018-04-27 12:03:47 +0200

    Update eslint styleguide to support es2017

--- eslint-config-5minds/index.js
+++ eslint-config-5minds/index.js
@@ -7,7 +7,7 @@ module.exports = {
     './rules/airbnb/node',
     './rules/airbnb/style',
     './rules/airbnb/variables',
-    './rules/airbnb/es6',
+    './rules/airbnb/es2017',
     './rules/airbnb/imports',
     './rules/5minds_rules',
   ].map(require.resolve),
